884694998e56c289ccde6f000ec2fea8fabbf7b2
yuszhaqDev <yuszhaq@gmail.com>
2024-06-28 17:05:38 +0100

    building a better router

--- views/about.view.php
+++ views/about.view.php
@@ -1,13 +1,13 @@
-<?php require ('partials/head.php')?>
+<?php require('partials/head.php') ?>
 
-<?php require ('partials/nav.php') ?>
+<?php require('partials/nav.php') ?>
 
-<?php require ('partials/banner.php') ?>
+<?php require('partials/banner.php') ?>
 
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>About Us</p>
-        </div>
-    </main>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Hello, Welcome to the about page</p>
+    </div>
+</main>
 
-<?php require ('partials/footer.php') ?>
+<?php require('partials/footer.php') ?>
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -13,6 +13,7 @@
         <p> <?= htmlspecialchars($note['body']) ?> </p>
 
         <form method="POST" class="mt-6">
+            <input type="hidden" name="_method" value="DELETE">
             <input type="hidden" name="id" value="<?= $note['id'] ?>">
             <button class="text-red-500 text-sm">Delete</button>
         </form>
