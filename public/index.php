72eec46ed253e4e1c7ca76ead75fa8d14b50cf71
yuszhaqDev <yuszhaq@gmail.com>
2024-07-29 11:27:07 +0100

    installing composer

--- public/index.php
+++ public/index.php
@@ -3,18 +3,15 @@
 use Core\Session;
 use Core\ValidationException;
 
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . '/vendor/autoload.php';
+
 session_start();
 
-const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'Core/function.php';
 
-spl_autoload_register(function ($class) {
-
-  $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-  require base_path("{$class}.php");
-});
 
 require base_path('bootstrap.php');
 
